7528d6290dd1b5c33c5f04e0a0525f5d95330ecc
Lea <lea.rerolle@gmail.com>
2018-03-21 00:40:46 +0100

    mardi soir

---
+++ exJS/TP-todolist/script.js
@@ -0,0 +1,16 @@
+// 1. fonction pour ajouter à la liste 
+// 2. fonction pour supprimer
+// Chacune de ces fonctions va entraîner une update de la liste et affichage
+
+
+function getToDo() {
+    return localStorage.getItem('todolist');
+}
+
+function addToDo() {
+
+}
+
+function deleteToDo() {
+
+}
