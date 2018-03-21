ee9242dd1248fa8a9bd3b275a9b2ad869ebc0bfa
Lea Rerolle <lea.rerolle@gmail.com>
2018-03-21 10:57:32 +0100

    TP todolist add, show & delete ok

--- exJS/TP-todolist/script.js
+++ exJS/TP-todolist/script.js
@@ -1,16 +1,48 @@
-// 1. fonction pour ajouter à la liste 
-// 2. fonction pour supprimer
-// Chacune de ces fonctions va entraîner une update de la liste et affichage
-
-
 function getToDo() {
-    return localStorage.getItem('todolist');
+    var todolist = new Array;
+    var todo_item = localStorage.getItem('todo');
+    if (todo_item != null) {
+        todolist = JSON.parse(todo_item);
+    }
+    return todolist;
 }
 
 function addToDo() {
+    var task = document.getElementById('todoinput').value; 
+    var todolist = getToDo();
+    todolist.push(task);
+    localStorage.setItem('todo', JSON.stringify(todolist));
+    showTodo();
 
+    return false;
+    
 }
 
 function deleteToDo() {
+    var id = this.getAttribute('id');
+    var todolist = getToDo();
+    todolist.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todolist));
+    showTodo();
+
+}    
+
+function showTodo() {
+    var todolist = getToDo();
+
+    var display = '<ul>';
+    for(var i=0; i<todolist.length; i++) {
+        display += '<li>' + todolist[i] + '<button class="delete" id="' + i  + '">x</button></li>';
+    };
+    display += '</ul>';
+ 
+    document.getElementById('todolist').innerHTML = display;
+    var deletebutton = document.getElementsByClassName('delete');
+    for (var i=0; deletebutton.length; i++) {
+        deletebutton[i].addEventListener('click', deleteToDo);
+    }
 
 }
+document.getElementById('todoadd').addEventListener('click', addToDo);
+showTodo();
+
